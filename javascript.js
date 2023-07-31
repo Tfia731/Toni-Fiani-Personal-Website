93424605d3304111237a7e89c2cc09843abee4a6
Toni <135765645+Tfia731@users.noreply.github.com>
2023-07-31 17:17:53 -0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Toni Fiani</title>
+
+    <link rel="formsubmit" type="text/html" href="formsubmit.html" />
+
+    </head>
+
+<body>
+<h2>Toni's Website</h2>
+<p>Have you filled out the contact form?</p>
+<script>
+
+<button onclick="alert (Thank you! Hope to chat soon!)">Yes</button>
+
+</script>
+</body>
+</html>
